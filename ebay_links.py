20fb63946480f8a1fc3d94b1c2069c5bc51d4d5c
fgscivittaro <fgscivittaro@uchicago.edu>
2016-08-17 15:04:15 -0500

    Update ebay_links.py
    This code scrapes the eBay homepage, finds the URLs of all the Featured Collections, then goes into each of those links and finds the URLs for all the individual products within each collection. 
    
    However, the scraper, as currently constructed, only scrapes some of the links (specifically, it only scrapes the first 12 out of 17 for each featured collection), even though every link is found within the same HTML tags and has the same attributes. Looking at the HTML code, the only difference I found is that the first 12 links and the final 5 links are separated by that chunk of XML code that I have included at the bottom, under the variable name potential_problem. Is this code the reason my scraper is not scraping the last five?

--- ebay_links.py
+++ ebay_links.py
@@ -1,23 +1,54 @@
 import requests
 from bs4 import BeautifulSoup
 
-#Scrapes the eBay home page and returns a list of links.
+#Scrapes the eBay home page and returns a list of links from each Featured Collection page.
 url = 'http://www.ebay.com/'
 soup = BeautifulSoup(requests.get(url).text, 'html.parser')
 
 no_lazy = soup.find_all('div', attrs = {'class':'no-lazy'})
-link_list = []
+featured_links = []
 
 for link in no_lazy:
-    link_list.append(link.find('a').get('href'))
+    featured_links.append(link.find('a').get('href'))
 
-product_list = []
+product_links = []
 
-for link in link_list:
+for link in featured_links:
     soup = BeautifulSoup(requests.get(link).text, 'html.parser')
     #For some reason, this does not return every single item.
     item_thumb = soup.find_all('div', attrs={'class':'itemThumb'})
     for link in item_thumb:
-        product_list.append(link.find('a').get('href'))
+        product_links.append(link.find('a').get('href'))
 
-print product_list
+print product_links
+
+potential_problem = """<script escape-xml="true">
+	  if (typeof(collectionState) != 'object') {
+	      var collectionState = {
+	          itemImageSize: {sWidth: 280, sHeight: 280, lWidth: 580, lHeight: 620},
+	          page: 1,
+	          totalPages: 2,
+	          totalItems: 17,
+	          pageId: '2057253',
+	          currentUser: '',
+	          collectionId: '324079803018',
+	          serviceHost: 'svcs.ebay.com/buying/collections/v1',
+	          owner: 'ebayhomeeditor',
+	          csrfToken: '',
+	          localeId: 'en-US',
+	          siteId: 'EBAY-US',
+	          countryId: 'US',
+	          collectionCosEnabled: 'true',
+	          collectionCosHostExternal: 'https://api.ebay.com/social/collection/v1',
+	          collectionCosEditEnabled: 'true',
+	          isCollectionReorderEnabled: 'false',
+	          isOwnerSignedIn: false || false,
+	          partiallySignedInUser: '@@__@@__@@',
+	          baseDomain: 'ebay.com',
+	          currentDomain: 'www.ebay.com',
+	          isTablet: false,
+	          isMobile: false,
+              showViewCount: true
+	      };
+	  }
+	</script>"""
