c5283879e9211eca317c1242f02f1fce011b8f66
fgscivittaro <fgscivittaro@uchicago.edu>
2016-08-17 15:06:08 -0500

    Update ebay_links.py
    This code scrapes the eBay homepage, finds the URLs of all the Featured Collections, then goes into each of those links and finds the URLs for all the individual products within each collection. 
    
    However, the scraper, as currently constructed, only scrapes some of the links (specifically, it only scrapes the first 12 out of 17 for each featured collection), even though every link is found within the same HTML tags and has the same attributes. Looking at the HTML code, the only difference I found is that the first 12 links and the final 5 links are separated by that chunk of XML code that I have included at the bottom, under the variable name potential_problem. Is this code the reason my scraper is not scraping the last five?

--- ebay_links.py
+++ ebay_links.py
@@ -1,23 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
 
-#Scrapes the eBay home page and returns a list of links.
+#Scrapes the eBay home page and returns a list of links from the home page's Featured Collections.
 url = 'http://www.ebay.com/'
 soup = BeautifulSoup(requests.get(url).text, 'html.parser')
 
 no_lazy = soup.find_all('div', attrs = {'class':'no-lazy'})
-link_list = []
+featured_links = []
 
 for link in no_lazy:
-    link_list.append(link.find('a').get('href'))
+    featured_links.append(link.find('a').get('href'))
 
-product_list = []
+product_links = []
 
-for link in link_list:
+for link in featured_links:
     soup = BeautifulSoup(requests.get(link).text, 'html.parser')
     #For some reason, this does not return every single item.
     item_thumb = soup.find_all('div', attrs={'class':'itemThumb'})
     for link in item_thumb:
-        product_list.append(link.find('a').get('href'))
+        product_links.append(link.find('a').get('href'))
 
-print product_list
+print product_links
