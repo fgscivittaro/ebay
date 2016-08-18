4a0fb9b60d4bf6aaa21ecb1be8ab0b357bca3fc4
Federico Scivittaro <fgscivittaro@uchicago.edu>
2016-08-18 14:11:40 -0500

    Update ebay_links.py

--- ebay_links.py
+++ ebay_links.py
@@ -12,43 +12,25 @@ for link in no_lazy:
     featured_links.append(link.find('a').get('href'))
 
 product_links = []
+final_links = []
 
-for link in featured_links:
-    soup = BeautifulSoup(requests.get(link).text, 'html.parser')
-    #For some reason, this does not return every single item.
-    item_thumb = soup.find_all('div', attrs={'class':'itemThumb'})
+for html_url in featured_links:
+    html_soup = BeautifulSoup(requests.get(html_url).text, 'html.parser')
+
+    editor = html_url[24:]
+    slash = editor.index('/')
+    editor = editor[:slash]
+    col_code = html_url[-12:]
+
+    lxml_url = 'http://www.ebay.com/cln/_ajax/2/%s/%s' % (editor, col_code)
+    params = {'itemsPerPage':'30'}
+    lxml_soup = BeautifulSoup((requests.get(lxml_url, params=params).content), 'lxml')
+
+    item_thumb = html_soup.find_all('div', attrs={'class':'itemThumb'})
     for link in item_thumb:
         product_links.append(link.find('a').get('href'))
 
-return product_links
+    final_links = [a["href"] for a in lxml_soup.select("div.itemThumb div.itemImg.image.lazy-image a[href]")]
 
-potential_problem = """<script escape-xml="true">
-	  if (typeof(collectionState) != 'object') {
-	      var collectionState = {
-	          itemImageSize: {sWidth: 280, sHeight: 280, lWidth: 580, lHeight: 620},
-	          page: 1,
-	          totalPages: 2,
-	          totalItems: 17,
-	          pageId: '2057253',
-	          currentUser: '',
-	          collectionId: '323101965012',
-	          serviceHost: 'svcs.ebay.com/buying/collections/v1',
-	          owner: 'ebaytecheditor',
-	          csrfToken: '',
-	          localeId: 'en-US',
-	          siteId: 'EBAY-US',
-	          countryId: 'US',
-	          collectionCosEnabled: 'true',
-	          collectionCosHostExternal: 'https://api.ebay.com/social/collection/v1',
-	          collectionCosEditEnabled: 'true',
-	          isCollectionReorderEnabled: 'false',
-	          isOwnerSignedIn: false || false,
-	          partiallySignedInUser: '@@__@@__@@',
-	          baseDomain: 'ebay.com',
-	          currentDomain: 'www.ebay.com',
-	          isTablet: false,
-	          isMobile: false,
-              showViewCount: true
-	      };
-	  }
-	</script>"""
+    product_links = list(set(product_links + final_links))
+    print len(product_links)
