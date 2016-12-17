6a50ec7b5d61660c29841b39e12b6acf85a6a8fd
Federico Scivittaro <fgscivittaro@uchicago.edu>
2016-12-16 19:37:53 -0600

    Edited the class in sale.py

--- sale.py
+++ sale.py
@@ -18,27 +18,35 @@ class Sale(object):
     def transaction_id(self):
         return self._transaction_id
 
+
     @property
     def datetime(self):
         return self._datetime
+
 
     @property
     def price(self):
         return self._price
 
+
     @property
     def quantity(self):
         return self._quantity
+
 
     @property
     def color(self):
         return self._color
 
 
-    def __repr__(self):
+    def __str__(self):
         return ("ID: {}, Date and Time: {}, Price: {}, Quantity: {}, Color: {}"
                 .format(self.transaction_id,
                         self.datetime,
                         self.price,
                         self.quantity,
                         self.color))
+
+
+    def __repr__(self):
+        return str(self)
